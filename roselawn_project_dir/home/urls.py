eeadc532314bae06ff8f8e62f6e085eec904aa39
Chitiiran <chitii91@gmail.com>
2024-02-15 09:44:56 -0500

    tmp

--- roselawn_project_dir/home/urls.py
+++ roselawn_project_dir/home/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import *
-from .views import PortfolioView
+
 urlpatterns = [
     path('', landing_page),
-    # path('portfolio/', portfolio),
-    # path('contact/', contact),
-    # path('test/', testtest),
+    path('portfolio/', portfolio),
+    path('contact/', contact),
+    path('test/', testtest),
     path('serial/', PortfolioView.as_view(), name='namenamename')
 ]
